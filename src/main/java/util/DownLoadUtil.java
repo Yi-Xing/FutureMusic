8fe4f42f305119797adb3f2d1338d5d9667bbe92
jjy <940140976@qq.com>
2019-05-12 17:42:59 +0800

    jjy

---
+++ src/main/java/util/DownLoadUtil.java
@@ -0,0 +1,11 @@
+package util;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DownLoadUtil {
+    public static int download(Object[] params, HttpServletRequest request, HttpServletResponse response){
+
+        return 0;
+    }
+}
