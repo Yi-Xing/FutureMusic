d4656da9e86a0c8fd55b7ed99d33c12f38fcf0c0
byh <734004037@qq.com>
2019-05-21 19:35:32 +0800

    -对左右布局宽度调整 -player页面左边目录基本完成 -Main类返回页面改为player

--- src/main/webapp/static/js/player.js
+++ src/main/webapp/static/js/player.js
@@ -1,14 +1,3 @@
-function tanK_bg() {
-    var tanK = document.getElementById('tanK');
-    var tanK_bg = document.getElementById('tanK_bg');
-    tanK_bg.style.display = 'inline-block';
-    document.onclick = function () {
-        var e = e || window.event;
-        var target = e.target || e.srcElement;
-        if (target.id == tanK_bg_con) {
-            return false;
-        } else {
-            tanK.style.display = 'none';
-        }
-    };
-}
+$('.icon-like').on('click',function () {
+    $(this).css('color','#F06868');
+});
