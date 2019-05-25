205557432da7869c04e5c9050276c9858db13be5
zyx <1160589090@qq.com>
2019-05-25 22:09:47 +0800

    页面

--- src/main/java/controller/user/consumer/Transaction.java
+++ src/main/java/controller/user/consumer/Transaction.java
@@ -13,13 +13,13 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class Transaction {
     /**
-     * 充值按钮执行此方法
+     * 充值按钮执行此方法，输入充值的金额
      *
      * @param money   给该邮箱号发送验证码
      * @param session   获取当前会话
      */
     public State repaidBalance(HttpSession session,String money){
-
+        // 开始付款，将数据传入该页面alipay.trade.page.pay.jsp
         return null;
     }
 }
