91a2008837b218383e2cf54b105ea8330a8845bd
zyx <1160589090@qq.com>
2019-05-29 19:49:55 +0800

    bug解决

--- src/main/java/util/exception/ExceptionResolver.java
+++ src/main/java/util/exception/ExceptionResolver.java
@@ -1,15 +1,19 @@
 package util.exception;
 
+import com.alipay.api.AlipayApiException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.text.ParseException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * 异常处理器
+ *
  * @author 5月13日 张易兴创建
  */
 public class ExceptionResolver implements HandlerExceptionResolver {
@@ -19,20 +23,27 @@ public class ExceptionResolver implements HandlerExceptionResolver {
     public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
         ModelAndView modelAndView = new ModelAndView();
         if (e instanceof DataBaseException) {
-             logger.debug("数据库异常"+o);
-            System.out.println("数据库异常"+o);
-            System.out.println("异常信息"+e);
-            // 设置跳转的路径
-            modelAndView.setViewName("/js/www");
+            // DataBaseException 数据库异常
+            logger.debug("数据库异常" + o);
+            modelAndView.addObject("Exception", "出异常了");
+        } else if (e instanceof IOException) {
+            // IOException异常  文件上传下载
+            logger.debug("文件上传下载异常" + o);
+            modelAndView.addObject("Exception", "出异常了");
+        } else if (e instanceof ParseException) {
+            //ParseException  字符串转换日期异常
+            logger.debug("字符串转换日期异常" + o);
+            modelAndView.addObject("Exception", "出异常了");
+        } else if (e instanceof AlipayApiException) {
+            //AlipayApiException 阿里巴巴的支付异常
+            logger.debug("阿里巴巴的支付异常异常" + o);
+            modelAndView.addObject("Exception", "出异常了");
         } else {
-            // 补充捕获 ParseException异常
-            logger.debug("异常"+o);
-            System.out.println("异常"+o);
-            System.out.println("异常信息"+e);
-            modelAndView.addObject("a", "出异常了");
-            // 设置跳转的路径
-            modelAndView.setViewName("BackgroundManagementSystem");
+            // 其他异常
+            logger.debug("其他异常" + o);
+            modelAndView.addObject("Exception", "出异常了");
         }
+        modelAndView.setViewName("BackgroundManagementSystem");
         return modelAndView;
     }
 }
