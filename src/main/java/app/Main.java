8a5e6408aa2d1c2cbe9e900feece6e08d75831e8
byh <734004037@qq.com>
2019-07-23 22:05:50 +0800

    -歌词同步滚动完成 -主页换皮

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -88,6 +88,16 @@ public class Main {
         return "toEmail";
     }
 
+    @RequestMapping(value = "/notice")
+    public String notice() {
+        return "notice";
+    }
+
+    @RequestMapping(value = "/comment")
+    public String comment() {
+        return "comment";
+    }
+
 
     @RequestMapping(value = "/order")
     public String order() {
