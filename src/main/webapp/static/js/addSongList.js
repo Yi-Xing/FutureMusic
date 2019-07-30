87316c9a74c5190ebbade6b565b31bd8907b03be
byh <734004037@qq.com>
2019-07-30 17:30:12 +0800

    -搜索框重写 -mv收藏和取消收藏

---
+++ src/main/webapp/static/js/addSongList.js
@@ -0,0 +1,30 @@
+
+
+function addSongList(length,songList) {
+    var tips = "";
+    var tips_head = "<div class=\"modal fade\" id=\"addMusic\" tabindex=\"-1\"\n" +
+        "                     role=\"dialog\" aria-labelledby=\"myModalLabel\" aria-hidden=\"true\">\n" +
+        "                    <div class=\"modal-dialog\">\n" +
+        "                        <div class=\"modal-content\">\n" +
+        "                            <div class=\"modal-header\">\n" +
+        "                                <h4 class=\"modal-title\">\n" +
+        "                                    添加到歌单\n" +
+        "                                </h4>\n" +
+        "                            </div>\n" +
+        "                            <div class=\"modal-body\">";
+    //便离开始
+    var songList = songList;    //数组
+    var tips_1 = "<div class=\"form-group\">\n" +
+        "                                    <label class=\"col-sm-2 control-label\">";
+    // var songList[i].name;    //歌单i的名字
+    var tips_2 = "</label>\n" +
+        "                                    <div class=\"col-sm-9\">\n" +
+        "                                        <input type=\"radio\" id=\"musicList";
+    // var songList[i].id;     //歌单i的id
+    var tips_3 = "\" value=\"1\">\n" +
+        "                                    </div>\n" +
+        "                                </div>";
+    //遍历结束
+    var tips_foot = 
+
+}
