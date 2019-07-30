ceaedd690259e4279f5541cda8a2ad5a19a6c358
zyx <1160589090@qq.com>
2019-07-30 10:53:56 +0800

    搜索框

--- src/main/java/service/Emergency.java
+++ src/main/java/service/Emergency.java
@@ -42,7 +42,7 @@ public class Emergency {
     /**
      * 显示首页
      */
-    public String homePage( Model model) {
+    public String homePage(Model model) {
         // 3个活动信息 List<Activity>
         model.addAttribute("Activity", getActivity());
         // 5个音乐人，每个人5个音乐 Map<User, List<Music>>
@@ -50,21 +50,21 @@ public class Emergency {
         // 查找4个MV  Map<Integer, List<Music>>
         model.addAttribute("musicVideo", getMusicVideo());
         // 查找15首个分5份来存放  Map<Integer, List<Music>>
-        Map<Integer, List<Music>> music= getMusic();
+        Map<Integer, List<Music>> music = getMusic();
         model.addAttribute("music1", music.get(0));
         model.addAttribute("music2", music.get(1));
-        model.addAttribute("music3" ,music.get(2));
-        model.addAttribute("music4",music.get(3));
-        model.addAttribute("music5",music.get(4));
+        model.addAttribute("music3", music.get(2));
+        model.addAttribute("music4", music.get(3));
+        model.addAttribute("music5", music.get(4));
         return "music/index";
     }
 
     /**
      * 查找所有的音乐分页显示
      */
-    public String getMusicList(Integer page,String  condition,Model model){
-        Music music=new Music();
-        if(condition!=null && !"".equals(condition)){
+    public String getMusicList(Integer page, String condition, Model model) {
+        Music music = new Music();
+        if (condition != null && !"".equals(condition)) {
             music.setName(condition);
         }
         //在查询之前传入当前页，然后多少记录
@@ -78,10 +78,11 @@ public class Emergency {
         model.addAttribute("condition", condition);
         return "music/music";
     }
+
     /**
      * 查找所有的MV分页显示
      */
-    public String getMusicVideoList(Integer page, Model model){
+    public String getMusicVideoList(Integer page, Model model) {
         //在查询之前传入当前页，然后多少记录
         PageHelper.startPage(page, 18);
         // 根据条件查找MV信息
@@ -94,13 +95,14 @@ public class Emergency {
         model.addAttribute("pageCount", pageInfo.getNavigatepageNums().length);
         return "music/musicVideo";
     }
+
     /**
      * 查找所有的专辑分页显示
      */
-    public String getSongListList(Integer page, Model model){
+    public String getSongListList(Integer page, Model model) {
         //在查询之前传入当前页，然后多少记录
         PageHelper.startPage(page, 18);
-        SongList songList=new SongList();
+        SongList songList = new SongList();
         songList.setType(2);
         // 根据条件查找专辑信息
         List<SongList> list = songListMapper.selectListSongList(songList);
@@ -137,7 +139,7 @@ public class Emergency {
         List<User> listUsers = userMapper.selectUsers(user);
         List<User> listUser = new ArrayList<>();
         // 得到5个用户
-        System.out.println("个数"+listUsers.size());
+        System.out.println("个数" + listUsers.size());
         if (listUsers.size() >= 5) {
             listUser.add(listUsers.get(0));
             listUser.add(listUsers.get(1));
@@ -149,7 +151,7 @@ public class Emergency {
         // 用来存储每个用户的5个音乐
         // 遍历用户查找音乐
         for (User u : listUser) {
-        List<Music> listMusic = new ArrayList<>();
+            List<Music> listMusic = new ArrayList<>();
             music.setSingerId(u.getId());
             // 查找到指定用户的所有音乐
             List<Music> list = musicMapper.selectListMusic(music);
@@ -177,27 +179,27 @@ public class Emergency {
             listMusic.add(list.get(0));
             listMusic.add(list.get(1));
             listMusic.add(list.get(2));
-            map.put(0,listMusic);
+            map.put(0, listMusic);
             listMusic = new ArrayList<>();
             listMusic.add(list.get(3));
             listMusic.add(list.get(4));
             listMusic.add(list.get(5));
-            map.put(1,listMusic);
+            map.put(1, listMusic);
             listMusic = new ArrayList<>();
             listMusic.add(list.get(6));
             listMusic.add(list.get(7));
             listMusic.add(list.get(8));
-            map.put(2,listMusic);
+            map.put(2, listMusic);
             listMusic = new ArrayList<>();
             listMusic.add(list.get(9));
             listMusic.add(list.get(10));
             listMusic.add(list.get(11));
-            map.put(3,listMusic);
+            map.put(3, listMusic);
             listMusic = new ArrayList<>();
             listMusic.add(list.get(12));
             listMusic.add(list.get(13));
             listMusic.add(list.get(14));
-            map.put(4,listMusic);
+            map.put(4, listMusic);
         }
         return map;
     }
@@ -205,10 +207,10 @@ public class Emergency {
     /**
      * 查找4个MV
      */
-    public List<MusicVideo> getMusicVideo(){
-        List<MusicVideo> musicVideoList=new ArrayList<>();
-        List<MusicVideo>  list=musicVideoMapper.selectListMusicVideo(new MusicVideo());
-        if(list.size()>=4){
+    public List<MusicVideo> getMusicVideo() {
+        List<MusicVideo> musicVideoList = new ArrayList<>();
+        List<MusicVideo> list = musicVideoMapper.selectListMusicVideo(new MusicVideo());
+        if (list.size() >= 4) {
             musicVideoList.add(list.get(0));
             musicVideoList.add(list.get(1));
             musicVideoList.add(list.get(2));
@@ -216,20 +218,21 @@ public class Emergency {
         }
         return musicVideoList;
     }
+
     /**
      * 判断指定音乐是否已被收藏  1为音乐   （1为收藏  0为没收藏）
      */
-    public List<Music> getMusicCollect(User user,List<Music> list){
-        MusicCollect musicCollect=new MusicCollect();
+    public List<Music> getMusicCollect(User user, List<Music> list) {
+        MusicCollect musicCollect = new MusicCollect();
         // 设置用户Id
         musicCollect.setUserId(user.getId());
         musicCollect.setType(1);
-        for(Music music:list){
+        for (Music music : list) {
             musicCollect.setMusicId(music.getId());
-            int count =musicCollectMapper.selectUserMusicCollectCount(musicCollect);
-            if(count!=0){
+            int count = musicCollectMapper.selectUserMusicCollectCount(musicCollect);
+            if (count != 0) {
                 music.setClassificationId(1);
-            }else {
+            } else {
                 music.setClassificationId(0);
             }
         }
@@ -239,17 +242,17 @@ public class Emergency {
     /**
      * 判断指定MV是否已被收藏  2为MV
      */
-    public List<MusicVideo> getMusicVideoCollect(User user,List<MusicVideo> list){
-        MusicCollect musicCollect=new MusicCollect();
+    public List<MusicVideo> getMusicVideoCollect(User user, List<MusicVideo> list) {
+        MusicCollect musicCollect = new MusicCollect();
         // 设置用户Id
         musicCollect.setUserId(user.getId());
         musicCollect.setType(2);
-        for(MusicVideo music:list){
+        for (MusicVideo music : list) {
             musicCollect.setMusicId(music.getId());
-            int count =musicCollectMapper.selectUserMusicCollectCount(musicCollect);
-            if(count!=0){
+            int count = musicCollectMapper.selectUserMusicCollectCount(musicCollect);
+            if (count != 0) {
                 music.setClassificationId(1);
-            }else {
+            } else {
                 music.setClassificationId(0);
             }
         }
@@ -260,13 +263,13 @@ public class Emergency {
     /**
      * 判断指定专辑或歌单是否收藏
      */
-    public SongList getSongListCollect(User user,SongList songList){
-        SongListCollect songListCollect=new SongListCollect();
+    public SongList getSongListCollect(User user, SongList songList) {
+        SongListCollect songListCollect = new SongListCollect();
         songListCollect.setUserId(user.getId());
         songListCollect.setMusicId(songList.getId());
-        if(songListCollectMapper.selectUserSongListCollectCount(songListCollect)!=0){
+        if (songListCollectMapper.selectUserSongListCollectCount(songListCollect) != 0) {
             songList.setClassificationId(1);
-        }else {
+        } else {
             songList.setClassificationId(0);
         }
         return songList;
@@ -275,10 +278,22 @@ public class Emergency {
     /**
      * 模糊查找指定音乐
      */
-    public List<Music> getSearchMusic(String search){
-        Music music=new Music();
+    public List<Music> getSearchMusic(String search) {
+        Music music = new Music();
         music.setName(search);
-        return musicMapper.selectListMusic(music);
+        List<Music> list = new ArrayList<>();
+        // 循环遍历控制循环次数最多5次
+        int i = 1;
+        for (Music m : musicMapper.selectListMusic(music)) {
+            if (i <= 5) {
+                list.add(m);
+                i++;
+            } else {
+                break;
+            }
+        }
+        logger.debug("搜索到的内容"+list);
+        return list;
     }
 
 }
